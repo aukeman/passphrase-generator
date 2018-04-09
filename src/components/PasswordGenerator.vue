79e8d163c650f4e04787cab4db42fa9f7bbd74f5
Jake Aukeman <jacob@jacobaukeman.com>
2018-04-09 06:28:41 -0400

    download word corpus

--- src/components/PasswordGenerator.vue
+++ src/components/PasswordGenerator.vue
@@ -4,6 +4,7 @@
       <password-input name="Minimum Length" v-model="options.length" />
       <password-input name="Special Characters" v-model="options.specialChars" />
       <password-input name="Upper Case" v-model="options.capitals" />
+      <div>number of words in corpus: {{words.length}}</div>
   </div>
 </template>
 
@@ -21,8 +22,14 @@ export default {
   data: function () {
     return {
       passwords: ['abc', 'def', 'ghi', 'jkl'],
-      options: { length: 25, specialChars: 50, capitals: 75 }
+      options: { length: 25, specialChars: 50, capitals: 75 },
+      words: []
     }
+  },
+  created: function () {
+    this.$http.get('static/dictionary.txt').then(function (response) {
+      this.words = response.body.split('\n')
+    })
   }
 }
 </script>
