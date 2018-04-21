b2d966c5049ed1ed7ad2b6824523dbecf5b23b79
Jake Aukeman <jacob@jacobaukeman.com>
2018-04-21 16:57:46 -0400

    take words from word database

--- src/components/PasswordGenerator.vue
+++ src/components/PasswordGenerator.vue
@@ -5,6 +5,12 @@
       <password-input name="Special Characters" v-model="options.specialChars" />
       <password-input name="Upper Case" v-model="options.capitals" />
       <div>number of words in corpus: {{words.length}}</div>
+      <div>minimum length: {{min_length}}</div>
+      <div>maximum length: {{max_length}}</div>
+      <div>minimum syllables: {{min_syllables}}</div>
+      <div>maximum syllables: {{max_syllables}}</div>
+      <div>minimum frequency: {{min_frequency}}</div>
+      <div>maximum frequency: {{max_frequency}}</div>
   </div>
 </template>
 
@@ -24,10 +30,27 @@ export default {
     return {
       number_of_passwords: 5,
       options: { length: 4, specialChars: 50, capitals: 75 },
-      words: []
+      words_db: {},
+
+      min_length: 0,
+      max_length: 0,
+
+      min_syllables: 0,
+      max_syllables: 0,
+
+      min_frequency: 0,
+      max_frequency: 0
     }
   },
   computed: {
+    words: function () {
+      var self = this;
+      if (Object.keys(self.words_db).length === 0) {
+        return [];
+      } else {
+        return Object.keys(self.words_db);
+      }
+    },
     passwords: function () {
       var self = this
       if (self.words.length === 0) {
@@ -43,7 +66,30 @@ export default {
   },
   created: function () {
     this.$http.get('static/dictionary.txt').then(function (response) {
-      this.words = response.body.split('\n')
+      var lines = response.body.split('\n')
+
+      var minValues = lines.shift().split(' ')
+
+      this.min_length = minValues[0]
+      this.min_syllables = minValues[1]
+      this.min_frequency = minValues[2]
+
+      var maxValues = lines.shift().split(' ')
+
+      this.max_length = maxValues[0]
+      this.max_syllables = maxValues[1]
+      this.max_frequency = maxValues[2]
+
+      this.words_db = _.reduce(lines,
+                               function (a, e) {
+                                 var values = e.split(' ');
+                                 a[values[0]] = {
+                                   length: values[1],
+                                   syllables: values[2],
+                                   frequency: values[3]}
+                                 return a
+        },
+        {})
     })
   }
 }
